147cd49eadd3935b1c3c40124542c9262b5218a9
Axel <atthehotcorner@users.noreply.github.com>
2017-02-28 21:22:36 -0500

    Template

--- index.android.js
+++
@@ -1,8 +0,0 @@
-/*import React from 'react';
-import {AppRegistry} from 'react-native';
-
-import App from './src/components/App';
-
-AppRegistry.registerComponent('encorelist', () => App);*/
-
-import './src/components/App';
